1918993cfe9c127caf9afc4623d486476db74e3f
Alexey Khit <alexey.khit@gmail.com>
2023-09-07 17:17:11 +0300

    Add multiple connect tries

--- custom_components/jura/core/__init__.py
+++ custom_components/jura/core/__init__.py
@@ -10,15 +10,19 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "jura"
 
 
-async def start_product(device: Device):
-    try:
-        command = device.command()
-        data = encdec(command, device.key)
+async def start_product(device: Device) -> bool:
+    command = device.command()
+    data = encdec(command, device.key)
 
-        async with BleakClient(device.mac) as client:
-            _LOGGER.debug(f"start product {command.hex()} => {data.hex()}")
-            await client.write_gatt_char(
-                "5a401525-ab2e-2548-c435-08c300000710", data, response=True
-            )
-    except Exception as e:
-        _LOGGER.exception(f"start product {e}")
+    for _ in range(4):
+        try:
+            async with BleakClient(device.mac) as client:
+                _LOGGER.debug(f"start product {command.hex()} => {data.hex()}")
+                await client.write_gatt_char(
+                    "5a401525-ab2e-2548-c435-08c300000710", data, response=True
+                )
+                return True
+        except:
+            pass
+
+    return False
