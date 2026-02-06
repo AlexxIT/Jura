09e25954f5e019a569927a483a237cf44bb40bcd
Nyctobello <125308849+Nyctobello@users.noreply.github.com>
2026-02-06 10:20:18 -0500

    Entity ID validation for HA 2026.2 workaround
    Commenting out line 25 in entity.py

--- custom_components/jura/core/entity.py
+++ custom_components/jura/core/entity.py
@@ -22,7 +22,7 @@ class JuraEntity(Entity):
         self._attr_name = device.name + " " + attr.replace("_", " ").title()
         self._attr_unique_id = device.mac.replace(":", "") + "_" + attr
 
-        self.entity_id = DOMAIN + "." + self._attr_unique_id
+        #self.entity_id = DOMAIN + "." + self._attr_unique_id
 
         self.internal_update()
 
